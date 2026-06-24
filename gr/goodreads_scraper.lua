4d536d5d98e67a49ff734f9982dbbf5f9f419c53
Michael Daines <mdaines@adobe.com>
2026-06-24 13:59:59 -0700

    Handle restart after error or abort

--- gr/goodreads_scraper.lua
+++ gr/goodreads_scraper.lua
@@ -17,27 +17,54 @@ local path = "../../kiroku/data/eyebooks.md"
 
 local books = data.parse(path)
 local outfile = "/tmp/" .. path:gsub(".*/", "")
-local fout = io.open(outfile, "a+")
 local info, err
 
-if path:find("audio") then
-	fout:write("| title | author | year | country | pages | hours | tags | rating | # ratings | goodreads |\n")
-	fout:write("| --- | --- | :---: | --- | ---: | ---: | --- | :---: | ---: | --- |\n")
-else
-	fout:write("| title | author | year | country | pages | tags | rating | # ratings | goodreads |\n")
-	fout:write("| --- | --- | :---: | --- | ---: | --- | :---: | ---: | --- |\n")
+local resume_from = 1
+local fcheck = io.open(outfile, "r")
+if fcheck then
+	local last_title
+	for line in fcheck:lines() do
+		local title = line:match("^| (.-) |")
+		if title and title ~= "title" and not title:find("^[%- :]+$") then
+			last_title = title
+		end
+	end
+	fcheck:close()
+	if last_title then
+		for i, book in ipairs(books) do
+			if book.title == last_title then
+				resume_from = i + 1
+				break
+			end
+		end
+		print("Resuming from book " .. resume_from .. " (after \"" .. last_title .. "\")")
+	end
+end
+
+local fout = assert(io.open(outfile, resume_from == 1 and "w" or "a"))
+
+if resume_from == 1 then
+	if path:find("audio") then
+		fout:write("| title | author | year | country | pages | hours | tags | rating | # ratings | goodreads |\n")
+		fout:write("| --- | --- | :---: | --- | ---: | ---: | --- | :---: | ---: | --- |\n")
+	else
+		fout:write("| title | author | year | country | pages | tags | rating | # ratings | goodreads |\n")
+		fout:write("| --- | --- | :---: | --- | ---: | --- | :---: | ---: | --- |\n")
+	end
 end
 
 for i, book in ipairs(books) do
+	if i < resume_from then goto continue end
+
 	-- print(string.format("%3d", i) .. " " .. book.title)
 
 	info, err = scraper.audit_book(book)
 
-	if tonumber(book.rating) and math.abs(book.rating - (info.rating or book.rating)) > 0.02 then
-		print(string.format("%3d", i) .. " " .. book.title .. ", rating: " .. book.rating .. " -> " .. info.rating)
-	end
-
 	if info then
+		if tonumber(book.rating) and math.abs(book.rating - (info.rating or book.rating)) > 0.02 then
+			print(string.format("%3d", i) .. " " .. book.title .. ", rating: " .. book.rating .. " -> " .. info.rating)
+		end
+
 		local has_audio = book.hours or info.hours
 		local no_audio = false
 		for _, t in ipairs(book.tags) do
@@ -91,6 +118,7 @@ for i, book in ipairs(books) do
 	end
 
 	socket.sleep(0.2)
+	::continue::
 end
 
 fout:close()
